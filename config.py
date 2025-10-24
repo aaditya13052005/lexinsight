39b88f6a29d3981e2a7b0e30d0046fb7cf8d6525
aaditya dongre <sai13aadityad@gmail.com>
2025-10-24 10:18:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,4 +7,4 @@ HF_API_KEY = "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"
 HF_MODEL = "facebook/bart-large-cnn"
 
 # Optional: API URLs or other global configs
-HF_API_URL = f
+HF_API_URL = f"https://api-inference.huggingface.co/models/{HF_MODEL}"
