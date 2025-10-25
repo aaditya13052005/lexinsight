825a0c127f1a38ce2f9da121e71edb39882985c5
aaditya dongre <sai13aadityad@gmail.com>
2025-10-25 20:49:35 +0530

    Update and rename config.py to hf_config.py

---
+++ hf_config.py
@@ -0,0 +1,7 @@
+
+HF_TOKEN = "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" # replace with your real token
+# Make this change
+HF_EMBED_URL = "https://api-inference.huggingface.co/pipeline/feature-extraction/sentence-transformers/all-MiniLM-L6-v2"
+HF_SUMMARY_URL = "https://api-inference.huggingface.co/models/facebook/bart-large-cnn"
+
+COHERE_API_KEY = "gW5znzO9PiCv7SqqcKcekbgmOEM6TI8uw96XJoeE"
